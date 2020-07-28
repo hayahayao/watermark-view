7269ddeb6103f48198f23f2dfd067ab1c34b41d2
xiaohanwang <xiaohanwang@sohu-inc.com>
2020-07-28 16:21:57 +0800

    fix: 🔧 modify config for more color display

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
 chrome.runtime.onInstalled.addListener(function () {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { pathContains: 'zxkp-admin' },
-      })
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({ pageUrl: { pathContains: 'zxkp-admin' } }),
+          new chrome.declarativeContent.PageStateMatcher({ pageUrl: { hostContains: 'localhost' } }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
   });
 });
--- watermark.js
+++ watermark.js
@@ -8,7 +8,7 @@ chrome.runtime.onMessage.addListener(
         } else {
             mask = document.createElement('div')
             mask.id = 'mask'
-            mask.style = 'height: 100%; width: 100%; background: black; z-index: 10000000; mix-blend-mode: color-burn; position: absolute; left: 0; top: 0;'
+            mask.style = 'height: 100%; width: 100%; background: midnightblue; z-index: 10000000; mix-blend-mode: color-burn; position: absolute; left: 0; top: 0;'
             document.body.appendChild(mask)
         }
     })
